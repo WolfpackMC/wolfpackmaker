23e97e9b2555e84c48b54a0abd53908cb2bf715b
Kalka <k@kalka.io>
2021-04-04 17:58:48 -0400

    fix python directory lookup

--- wolfpackmaker.bat
+++ wolfpackmaker.bat
@@ -3,16 +3,16 @@
 echo Checking for Python...
 
 set install_dir=..\..\python
-set python_dir=%install_dir%\python.3.9.3
-set python_bin=%python_dir%\tools\python
+for /f "delims=" %%D in ('dir %install_dir% /a:d /b') do set python_dir=%%~nxD
+set python_bin=%install_dir%\%python_dir%\tools\python
 
 %python_bin% --version 2>NUL
 if errorlevel 1 (
     echo "Python not found. Grabbing NuGet..."
     if not exist nuget.exe powershell -Command "Invoke-WebRequest https://dist.nuget.org/win-x86-commandline/v5.9.0/nuget.exe -OutFile nuget.exe"
     nuget install python -o %install_dir%
-    nuget verify %python_dir%\python.3.9.3.nupkg -All
-    del %python_dir%\python.3.9.3.nupkg
+    nuget verify %python_dir%\%python_dir%.nupkg -All
+    del %python_dir%\%python_dir%.nupkg
 )
 
 %python_bin% -m pip install -r ..\wolfpackmaker\requirements.txt --no-warn-script-location
