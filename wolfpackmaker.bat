4fc17f2b582fa7baaccb196623d90b26ce5c5109
Kalka <k@kalka.io>
2021-04-04 06:47:48 -0400

    do this instead

--- wolfpackmaker.bat
+++ wolfpackmaker.bat
@@ -6,8 +6,7 @@ set install_dir=..\..\python
 set python_dir=%install_dir%\python.3.9.3
 set python_bin=%python_dir%\tools\python
 
-python --version 2>NUL
-if errorlevel 1 %python_bin% --version 2>NUL
+%python_bin% --version 2>NUL
 if errorlevel 1 (
     echo "Python not found. Grabbing NuGet..."
     if not exist nuget.exe powershell -Command "Invoke-WebRequest https://dist.nuget.org/win-x86-commandline/v5.9.0/nuget.exe -OutFile nuget.exe"
