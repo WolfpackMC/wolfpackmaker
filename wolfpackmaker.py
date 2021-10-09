4af175d208723b56621b0e582bf811372e1f1759
Kalka <k@kalka.io>
2021-10-09 07:47:40 -0400

    shift the error handler around

--- wolfpackmaker.py
+++ wolfpackmaker.py
@@ -105,16 +105,15 @@ modpack_version_cached = join(cached_dir, '.modpack_version.txt')
 
 
 async def save_mod(mod_filename, mod_downloadurl, session=aiohttp.ClientSession()):
-    async with session.get(mod_downloadurl) as r:
-        try:
-            r.raise_for_status()
-        except (ClientResponseError, ClientConnectorCertificateError) as e:
-            log.info(f"We were not able to download {mod_downloadurl} due to the ClientResponseError: {e}. We will retry the download using urllib3 (no guarantees)")
-            await asyncio.sleep(3)
-            save_mod_sync(mod_filename, mod_downloadurl)
-        with open(join(mods_cache_dir, mod_filename), 'wb') as f:
-            async for data in r.content.iter_chunked(65535):
-                f.write(data)
+    try:
+        async with session.get(mod_downloadurl) as r:
+            with open(join(mods_cache_dir, mod_filename), 'wb') as f:
+                async for data in r.content.iter_chunked(65535):
+                    f.write(data)
+    except (ClientResponseError, ClientConnectorCertificateError) as e:
+        log.info(f"We were not able to download {mod_downloadurl} due to: {e}. \nWe will retry the download using urllib3 (no guarantees)")
+        await asyncio.sleep(3)
+        save_mod_sync(mod_filename, mod_downloadurl)
 
 def save_mod_sync(mod_filename, mod_downloadurl):
     import urllib3
