b40f65c4c5ca8e574ff2219f507eaf36e74d7228
Kalka <k@kalka.io>
2021-10-11 08:51:32 -0400

    check for python again after it was installed, fixes installation bug

--- wolfpackmaker.bat
+++ wolfpackmaker.bat
@@ -1,9 +1,13 @@
 @echo off
 
+:CHECK_PYTHON
+for /f "delims=" %%D in ('dir %install_dir% /a:d /b') do set python_dir=%%~nxD
+EXIT /B 0
+
 echo Checking for Python...
 
 set install_dir=..\..\python
-for /f "delims=" %%D in ('dir %install_dir% /a:d /b') do set python_dir=%%~nxD
+goto :CHECK_PYTHON
 set python_bin=%install_dir%\%python_dir%\tools\python
 
 %python_bin% --version 2>NUL
@@ -11,6 +15,7 @@ if errorlevel 1 (
     echo "Python not found. Grabbing NuGet..."
     if not exist nuget.exe powershell -Command "Invoke-WebRequest https://dist.nuget.org/win-x86-commandline/v5.9.0/nuget.exe -OutFile nuget.exe"
     nuget install python -o %install_dir%
+    goto :CHECK_PYTHON
     nuget verify %python_dir%\%python_dir%.nupkg -All
     del %python_dir%\%python_dir%.nupkg
 )
