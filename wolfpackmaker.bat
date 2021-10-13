762a62bf8e75f61498ea4e1bb8bf2dd96678785b
Kalka <k@kalka.io>
2021-10-13 11:57:27 -0400

    fix up the batch file (the verifier never worked lol)

--- wolfpackmaker.bat
+++ wolfpackmaker.bat
@@ -1,12 +1,10 @@
 @echo off
 
 
-for /f "delims=" %%D in ('dir %install_dir% /a:d /b') do set python_dir=%%~nxD
-
 echo Checking for Python...
 
 set install_dir=..\..\python
-goto :CHECK_PYTHON
+for /f "delims=" %%D in ('dir %install_dir% /a:d /b') do set python_dir=%%~nxD
 set python_bin=%install_dir%\%python_dir%\tools\python
 
 %python_bin% --version 2>NUL
@@ -14,8 +12,10 @@ if errorlevel 1 (
     echo "Python not found. Grabbing NuGet..."
     if not exist nuget.exe powershell -Command "Invoke-WebRequest https://dist.nuget.org/win-x86-commandline/v5.9.0/nuget.exe -OutFile nuget.exe"
     nuget install python -o %install_dir%
-    for /f "delims=" %%D in ('dir %install_dir% /a:d /b') do set python_dir=%%~nxD
 )
+
+for /f "delims=" %%D in ('dir %install_dir% /a:d /b') do set python_dir=%%~nxD
+set python_bin=%install_dir%\%python_dir%\tools\python
 
 %python_bin% -m pip install -r ..\wolfpackmaker\requirements-client.txt --no-warn-script-location
 
