d00f3d03ed733c0f333c59babaf52f609cea8471
Kalka <k@kalka.io>
2022-03-18 16:00:36 -0400

    fix windows batch script

--- wolfpackmaker.bat
+++ wolfpackmaker.bat
@@ -1,15 +1,12 @@
 @echo off
 
 
-echo Checking for Python...
-
-set install_dir=..\..\python
+set install_dir=%INST_DIR%\python
 for /f "delims=" %%D in ('dir %install_dir% /a:d /b') do set python_dir=%%~nxD
 set python_bin=%install_dir%\%python_dir%\tools\python
 
 %python_bin% --version 2>NUL
 if errorlevel 1 (
-    echo "Python not found. Grabbing NuGet..."
     if not exist nuget.exe powershell -Command "Invoke-WebRequest https://dist.nuget.org/win-x86-commandline/v5.9.0/nuget.exe -OutFile nuget.exe"
     nuget install python -o %install_dir%
 )
